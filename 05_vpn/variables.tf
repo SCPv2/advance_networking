0efc19083edba4be3b09aec235004ff65e245419
SCPv2 <scp_nuri@multicampus.co.kr>
2025-08-12 08:43:38 +0900

    Update and rename main.tf to variables.tf

---
+++ 05_vpn/variables.tf
@@ -0,0 +1,164 @@
+
+########################################################
+# VPC 변수 정의
+########################################################
+variable "vpcs" {
+  description = "VPC for Creative Energy"
+  type = list(object({
+    name        = string
+    cidr        = string
+    description = optional(string)
+  }))
+  default = [
+    {
+      name        = "VPC1"
+      cidr        = "10.1.0.0/16"
+      description = "Primary VPC"
+    }
+  ]
+}
+
+########################################################
+# Subnet 변수 정의
+########################################################
+variable "subnets" {
+  description = "Subnet for Creative Energy"
+  type = list(object({
+    name        = string
+    cidr        = string
+    type        = string   # GENERAL | LOCAL | VPC_ENDPOINT
+    vpc_name    = string   
+    description = string
+  }))
+  default = [
+    {
+      name        = "Subnet11"
+      cidr        = "10.1.1.0/24"
+      type        = "GENERAL"
+      vpc_name    = "VPC1"
+      description = "Subnet for Creative Energy"
+    }
+  ]
+}
+
+########################################################
+# Key Pair 변수 정의
+########################################################
+variable "keypair_name" {
+  type        = string
+  description = "Key Pair to access VM"
+  default     = "stkey"
+}
+
+variable "keypair_output_path" {
+  type        = string
+  description = "Local Path for New Key Pair"
+  default     = "./mykey.pem"                               # 사용자가 미리 생성한 Key Pair 이름 적용
+}
+
+########################################################
+# Security Group 변수 정의
+########################################################
+variable "security_group_bastion" {
+    type        = string
+    default     = "bastionSG"
+  }
+
+variable "security_group_web" {
+    type        = string
+    default     = "nfsvmSG"
+  }
+
+########################################################
+# port 변수 정의
+########################################################  
+variable "port_names" {
+  type = object({
+    bastion   = string
+    nfsvm = string
+  })
+  default = {
+    bastion   = "bastion-port"
+    nfsvm     = "nfsvm-port"
+  }
+}
+
+########################################################
+# Virtual Server Standard Image 변수 정의
+########################################################
+variable "image_windows_os_distro" {
+  type        = string
+  default     = "windows"
+}
+
+variable "image_windows_scp_os_version" {
+  type        = string
+  default     = "2022 Std."
+}
+
+variable "image_rocky_os_distro" {
+  type        = string
+  default     = "rocky"
+}
+
+variable "image_rocky_scp_os_version" {
+  type        = string
+  default     = "9.4"
+}
+
+########################################################
+# Virtual Server 변수 정의
+########################################################
+
+variable "server_type_id" {
+  type    = string
+  default = "s1v1m2"
+}
+
+variable "vm_bastion" {
+  type = object({
+    name = string
+    description = string
+  })
+  default = {
+    name = "vm110w"
+    description = "Bastion Host"
+  }
+}
+
+variable "boot_volume_windows" {
+  type = object({
+    size                  = number
+    type                  = optional(string)
+    delete_on_termination = optional(bool)
+  })
+  default = {
+    size                  = 32
+    type                  = "SSD"
+    delete_on_termination = true
+  }
+}
+
+variable "vm_nfsvm" {
+  type = object({
+    name = string
+    description = string
+  })
+  default = {
+    name = "vm111r"
+    description = "VM for NFS"
+  }
+}
+
+variable "boot_volume_rocky" {
+  type = object({
+    size                  = number
+    type                  = optional(string)
+    delete_on_termination = optional(bool)
+  })
+  default = {
+    size                  = 16
+    type                  = "SSD"
+    delete_on_termination = true
+  }
+}
